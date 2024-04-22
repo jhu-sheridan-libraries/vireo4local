5ab15377ec5935fadeac815f02c2c5630d80c60c
jrmartino <jrm@jhu.edu>
2024-04-22 12:24:11 -0400

    update buildConfig.js to not use mock auth

--- overlay.sh
+++ overlay.sh
@@ -13,6 +13,7 @@ WORK_DIR=/opt/vireo/work/v4.2.6/Vireo/
 
 # Copy the updated files into our working project
 cp ${LOCAL_DIR}/.env ${WORK_DIR}
+cp ${LOCAL_DIR}/.wvr/buildConfig.js ${WORK_DIR}/.wvr
 cp ${LOCAL_DIR}/docker-compose.yml ${WORK_DIR}
 cp ${LOCAL_DIR}/Dockerfile ${WORK_DIR}
 cp ${LOCAL_DIR}/src/main/resources/* ${WORK_DIR}/src/main/resources
