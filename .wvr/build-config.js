633560d1cab4ee34b82288cc51e53b72ec1b3c6b
jrmartino <jrm@jhu.edu>
2024-04-24 09:22:27 -0400

    reset auth location

--- .wvr/build-config.js
+++ .wvr/build-config.js
@@ -40,7 +40,7 @@ config = {
       transform(content) {
         return content
           .toString()
-          .replace('${AUTH_SERVICE_URL}',  'window.location.protocol + \'//\' + window.location.host + window.location.base + \'/login/ShibbolethAuthentication/return'')
+          .replace('${AUTH_SERVICE_URL}',  'window.location.protocol + \'//\' + window.location.host + window.location.base + \'/login/ShibbolethAuthentication/return\'')
   //        .replace('${AUTH_SERVICE_URL}', '\'https://idp.jh.edu/idp/shibboleth\'')
           .replace('${STOMP_DEBUG}', 'false');
       },
