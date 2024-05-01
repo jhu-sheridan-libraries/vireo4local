c22d39addd0a229ab035676c68de5180f43fd5a6
jrmartino <jrm@jhu.edu>
2024-05-01 16:41:53 -0400

    reset auth location

--- .wvr/build-config.js
+++ .wvr/build-config.js
@@ -41,7 +41,7 @@ config = {
         return content
           .toString()
   //        .replace('${AUTH_SERVICE_URL}',  'window.location.protocol + \'//\' + window.location.host + window.location.base + \'/auth\'')
-          .replace('${AUTH_SERVICE_URL}', '/token')
+          .replace('${AUTH_SERVICE_URL}', '\'https://vireo-dev.library.jhu.edu/token\'')
           .replace('${STOMP_DEBUG}', 'false');
       },
     },
